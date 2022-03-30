6bbdee5cd7d5608afe8934e1a321cd6f10cb859b
Nash <91821078+nashanuwar@users.noreply.github.com>
2022-03-30 13:01:12 +0900

    Update castingtypes.c

--- castingtypes.c
+++ castingtypes.c
@@ -1,6 +1,7 @@
 # include <stdio.h>
 
 //everytime there is a char to int conversion, it always turns into ascii value
+//the program treats the initialized char as a symbol
 int main() {
     char character1;
     char character2;
@@ -17,11 +18,15 @@ int main() {
     character2 = (int)character2;
     printf("%i\n", character2);
     //outputs 56
-    //converts to ascii value to 
-//converting int 8 to char
-    integer1 = 8;
+    //converts the integer to ascii value
 
+//converting int 8 to char
+    integer1 = 65;
+    integer1 = (char)integer1;
+    printf("%c", integer1);
     return 0;
+    //outputs A
+    //converts the ascii value to ascii symbol
 
   
 }
